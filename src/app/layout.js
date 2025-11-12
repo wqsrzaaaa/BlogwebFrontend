279567e6771f99a33f41e5b92a0608a56639fed1
Waqas Raza <wqsrza07@gmail.com>
2025-11-12 14:57:16 +1100

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,40 +1,25 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Roboto_Mono, Ubuntu } from "next/font/google";
 import "./globals.css";
-import { Ubuntu } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 
-
-const ubuntu = Ubuntu({
-  subsets: ["latin"],
-  weight: ["400"], 
-});
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["400"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-geist-sans" });
+const robotoMono = Roboto_Mono({ subsets: ["latin"], variable: "--font-geist-mono" });
 
 export const metadata = {
   title: "Bloggedin - Share Your Stories",
   description: "Bloggedin - Share Your Stories with the World",
   icons: {
     icon: "/bloglogo.png",
-    shortcut: "/bloglogo.png", 
+    shortcut: "/bloglogo.png",
   },
 };
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={` ${ubuntu.className} antialiased`}
-      >
-       <ThemeProvider
+      <body className={`${ubuntu.className} antialiased`}>
+        <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem={true}
